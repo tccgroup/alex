331027085b4b97e390b121b3f2f70578673bb589
cappelll <luca.cappelletta@recordsure.com>
2017-01-19 13:51:35 +0000

    LSD-861 testAlexVAD.py added

--- alex/recordsure/testAlexVAD.py
+++ alex/recordsure/testAlexVAD.py
@@ -1,236 +1,166 @@
 #!/usr/bin/env python
 
-import argparse
 import sys
 import os
-import shutil
-import wave
-import logging
-import datetime
-import random
+import argparse
+from StringIO import StringIO
+#import librosa
 import numpy as np
+#import scipy.io.wavfile as scipyWav
+import matplotlib.pyplot as plt
+
+pathToAdd = os.path.join(os.path.dirname(sys.argv[0]), "../../../experiments/share/")
+sys.path.append(pathToAdd)
+
+from lsdpylib import phraselib
+from lsdpylib import lsdutils
+
 
 if __name__ == '__main__':
     import autopath
 
-
-from alex.utils.htk import *
+from alex.utils import htk
+from alex.tools.vad import train_vad_nn_theano as train
 from alex.ml import ffnn
 from alex.ml import tffnn
 
-from alex.components.vad.ffnn import FFNNVADGeneral
-from alex.ml.tffnn import TheanoFFNN
 
-weight_l2=1e-6
-batch_size= 500000 
+def mediumTermeWindow(prev_frames=15, next_frames=15, amplify_center_frame=4.0):
 
-max_frames = 1000000 #1000000
+    amp_min = 1.0 / float(amplify_center_frame)
+    amp_max = 1.0
+    amp_prev = [amp_min + (float(i) / prev_frames) * (amp_max - amp_min) for i in range(0, prev_frames)]
+    amp_next = [amp_min + (float(i) / next_frames) * (amp_max - amp_min) for i in range(next_frames-1, -1, -1 )]
 
-max_files = 1000000
-max_frames_per_segment = 0
-trim_segments = 0
-
-next_frames = 15
-prev_frames = 15
-
-usec0=0
-usedelta=False
-useacc=False
-mel_banks_only=1
-uselda = 0
+    return np.repeat(amp_prev  + [1.0] + amp_next, 1000 / (prev_frames + 1 + next_frames))
 
 
-global features_file_name
+def tmp_mlf(fInAudioName, pOut):
+
+    fName = lsdutils.getFileBasename(fInAudioName)
+    fOutMLFName = os.path.join(pOut, fName + ".tmp.mlf")
+
+    length = lsdutils.audioLength(fInAudioName)
+    phrase = phraselib.Phrase(start=0, end=length)
+    phrase.setWords("unknown")
+
+    phraselib.writeMLF( [(fInAudioName, [phrase])], filename=fOutMLFName)
+
+    return fOutMLFName
 
 
-def load_mlf(train_data_sil_aligned, max_files, max_frames_per_segment):
-    """ Loads a MLF file and creates normalised MLF class.
+def extractFeatures(fInAudioName, keep_mlf=False):
 
-    :param train_data_sil_aligned:
-    :param max_files:
-    :param max_frames_per_segment:
-    :return:
-    """
-    print "Loading %s" % train_data_sil_aligned
-    mlf = MLF(train_data_sil_aligned, max_files=max_files)
-    mlf.filter_zero_segments()
-    # map all sp, _noise_, _laugh_, _inhale_ to sil
-    mlf.sub('sp', 'sil')
-    mlf.sub('_noise_', 'sil')
-    mlf.sub('_laugh_', 'sil')
-    mlf.sub('_inhale_', 'sil')
-    mlf.sub('SIL', 'sil')
-    mlf.sub('SP', 'sil')
-    mlf.sub('_NOISE_', 'sil')
-    mlf.sub('_LAUGH_', 'sil')
-    mlf.sub('_INHALE_', 'sil')
-    # map everything except of sil to speech
-    mlf.sub('sil', 'speech', False)
-    mlf.merge()
-    #mlf_sil.times_to_seconds()
-    mlf.times_to_frames()
-    mlf.trim_segments(trim_segments)
-    mlf.shorten_segments(max_frames_per_segment)
+    next_frames = 15
+    prev_frames = 15
+    amplify_center_frame = 4.0
+    usec0=0
+    usedelta=False
+    useacc=False
+    mel_banks_only=1
 
-    return mlf
+    fOutMLFName = tmp_mlf(fInAudioName, pOut)
+    length = lsdutils.audioLength(fInAudioName)
+    mlf = train.load_mlf(fOutMLFName, 1, 1+int(length*100))
 
+    vta = htk.MLFMFCCOnlineAlignedArray(usec0=usec0,n_last_frames=0, usedelta = usedelta, useacc = useacc, mel_banks_only = mel_banks_only)
+    vta.append_mlf(mlf)
 
-def gen_features(speech_data, speech_alignment):
-    vta = MLFMFCCOnlineAlignedArray(usec0=usec0,n_last_frames=0, usedelta = usedelta, useacc = useacc, mel_banks_only = mel_banks_only)
-    sil_count = 0
-    speech_count = 0
-    for sd, sa in zip(speech_data, speech_alignment):
-        mlf_speech = load_mlf(sa, max_files, max_frames_per_segment)
-        vta.append_mlf(mlf_speech)
-        vta.append_trn(sd)
-
-        sil_count += mlf_speech.count_length('sil')
-        speech_count += mlf_speech.count_length('speech')
-
-    print "The length of sil segments:    ", sil_count
-    print "The length of speech segments: ", speech_count
+    pIn = os.path.dirname(fInAudioName)
+    vta.append_trn(os.path.join(pIn, '*.wav'))
 
     mfcc = vta.__iter__().next()
 
-    print "Features vector length:", len(mfcc[0])
-    input_size = len(mfcc[0])
+    test_x = np.array([frame for frame, _ in vta]).astype(np.float32)
 
-    crossvalid_x = []
-    crossvalid_y = []
-    train_x = []
-    train_y = []
-    i = 0
-    for frame, label in vta:
-        # downcast
-        if frame is not None:
-            frame = frame.astype(np.float32)
-        #        frame = frame - (10.0 if mel_banks_only else 0.0)
+    if keep_mlf is False:
+        os.remove(fOutMLFName)
 
-            crossvalid_x.append(frame)
-            if label == "sil":
-                crossvalid_y.append(0)
-            else:
-                crossvalid_y.append(1)
-
-    crossvalid_x = np.array(crossvalid_x).astype(np.float32)
-    crossvalid_y = np.array(crossvalid_y).astype('int32')
-#    train_x = np.array(train_x).astype(np.float32)
-#    train_y = np.array(train_y).astype('int32')
+    return test_x
 
 
-    # normalise the data
-    tx_m = np.mean(crossvalid_x, axis=0)
-    tx_std = np.std(crossvalid_x, axis=0)
+def saveFeatures(fOutFeatureName, features):
 
-    crossvalid_x -= tx_m
-    crossvalid_x /= tx_std
-
-
-    print 'Saving data to:', features_file_name
-    f = open(features_file_name, "wb")
-    np.save(f, crossvalid_x) 
-    np.save(f, crossvalid_y)
-    np.save(f, train_x)
-    np.save(f, train_y)
-    np.save(f, tx_m)
-    np.save(f, tx_std)
-    f.close()
-
-    return crossvalid_x, crossvalid_y, train_x, train_y, tx_m, tx_std
+#    fOutFeatureName = os.path.join(pOut, "mao.npy")
+    print 'Saving data to:', fOutFeatureName
+#    features, mlf = feats
+#    fName = lsdutils.getFileBasename(fInAudioName)
+    with open(fOutFeatureName, 'wb') as fOutFeature:
+        np.save(fOutFeature, features, allow_pickle=False)
+#        np.save(fOutFeature, mlf, allow_pickle=False)
 
 
-def get_accuracy(true_y, predictions_y):
-    """ Compute accuracy of predictions from the activation of the last NN layer, and the sil prior probability.
+def test_model(features, fInModelName, pOut, ID=None):
 
-    :param ds: the training dataset
-    :param a: activation from the NN using the ds datasat
-    """
-    acc = np.mean(np.equal(np.argmax(predictions_y, axis=1),true_y))*100.0
-    sil = (1.0-float(np.count_nonzero(true_y)) / len(true_y))*100.0
-
-    return acc, sil
-
-
-def test(fInModelName, speech_data, speech_alignment):
-    print
-    print datetime.datetime.now()
-    print
-    random.seed(0)
-
-
-    try:
-        f = open(features_file_name, "rb")
-        crossvalid_x = np.load(f) 
-        crossvalid_y = np.load(f)
-        train_x = np.load(f)
-        train_y = np.load(f)
-        tx_m = np.load(f)
-        tx_std = np.load(f)
-        f.close()
-    except IOError:
-        print speech_data
-        print speech_alignment
-        print features_file_name
-        crossvalid_x, crossvalid_y, train_x, train_y, tx_m, tx_std = gen_features(speech_data, speech_alignment)
-
-    input_size = crossvalid_x.shape[1] * (prev_frames + 1 + next_frames)
-
-    tx_m = np.tile(tx_m, prev_frames + 1 + next_frames)
-    tx_std = np.tile(tx_std, prev_frames + 1 + next_frames)
+    fOutVADName = os.path.join(pOut, fName + ".vad")
 
     e = tffnn.TheanoFFNN()
     e.load(fInModelName)
-    e.set_input_norm(tx_m, tx_std)
 
-    predictions_y, gold_y = e.predict(crossvalid_x, batch_size, prev_frames, next_frames, crossvalid_y)
+    features_m = np.mean(features, axis=0)
+    features_std = np.std(features, axis=0)
+    features -= features_m
+    features /= features_std
 
+    e.set_input_norm(features_m, features_std)
 
-    return predictions_y, gold_y
-
-def saveVAD(fOutVADName, predictions_y, gold_y=None):
-
-    if gold_y is None:
-        gold_y =['unk'] * len(predictions_y)
+    predictions_y = e.predict(features, batch_size=100000, prev_frames= 15, next_frames=15)
 
     with open(fOutVADName, 'w') as fOutVAD:
-        for p, g in zip(predictions_y, gold_y):
-            print >> fOutVAD, "{:.3f},{:.3f},{}".format(p[0], p[1], g)
+        for p in predictions_y:
+            print >> fOutVAD, "%.3f,%.3f" % (p[0], p[1])
+
+    return predictions_y
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description='Extracts features using ALEX.')
 
-    global crossvalid_frames, usec0
+    parser.add_argument('-i','--input', help='Either audio or dnn features input file.', required=True)
+    parser.add_argument('-m','--model', help='Input model file.', default=None)
+#    parser.add_argument('-l','--list', help='Input list of audio files.', default=None)
+    parser.add_argument('-o','--outdir', help='Output directory', required=True)
+    parser.add_argument('--plot', help='Plot results', default=False, action='store_true')
+    parser.add_argument('--keep-mlf', help='Keep temporary mlf file', default=False, action='store_true')
+    
+#    parser.add_argument('--logdir', help='Log file(s) directory path. Log file will contain all the parsing warnings and some transcript stats.', nargs='?', const=True, default=None)
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-i', '--input', help="Input audio file", required=True)
-    parser.add_argument('--mlf', help="Input mlf file", default=None)
-    parser.add_argument('-m', '--model', help="Input model file", required=True)
-    parser.add_argument('-o', '--outdir', help="Output directory", required=True)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
 
     args = parser.parse_args()
 
-    pOut = args.outdir
-    fInAudioName = args.input
-    fInMLFName = args.mlf
+    fInInputName = args.input
     fInModelName = args.model
+#    fInListName = args.list
+    pOut = args.outdir
+    doPlot = args.plot
+    keep_mlf = args.keep_mlf
 
-#    crossvalid_frames = int((0.20 * max_frames))
+#    window = mediumTermeWindow()
 
-    features_file_name = "%s/vad_sds_mfcc_mfr%d_mfl%d_mfps%d_ts%d_usec0%d_usedelta%d_useacc%d_mbo%d.npc" % \
-                         (pOut, max_frames, max_files, max_frames_per_segment, trim_segments, usec0, usedelta, useacc, mel_banks_only)
+    fName = lsdutils.getFileBasename(fInInputName)
 
+    if fInInputName.endswith('.wav') :
+        features = extractFeatures(fInInputName, keep_mlf=keep_mlf)
 
-    fOutVADName = os.path.join(pOut, "results.vad")
+        fOutMLFName = os.path.join(pOut, fName + ".dnnfeats")
+        saveFeatures(fOutMLFName, features)
 
-    train_speech = [fInAudioName]
-    train_speech_alignment = [fInMLFName]
+    elif fInInputName.endswith('.dnnfeats'):
+        with open(fInInputName, 'rb') as fInInput:
+            features = np.load(fInInput)
 
-    predictions_y, gold_y = test(fInModelName, train_speech, train_speech_alignment)
+    print features.shape
 
-    c_acc, c_sil = get_accuracy(gold_y, predictions_y)
+    if fInModelName is not None:
 
-    saveVAD(fOutVADName, predictions_y, gold_y)
+        predictions_y = test_model(features, fInModelName, pOut, ID=fName)
 
+        if doPlot is not False:
+            plt.plot(predictions_y[:,0], 'r')
+            plt.plot(predictions_y[:,1], 'b')
+            plt.show()
 
